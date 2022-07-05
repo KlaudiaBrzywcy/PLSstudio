ccee4f16af59b0d3d534fb485dd9e483b3f76caf
Klaudia Brzywcy <kbrzywcy@gmail.com>
2022-07-05 17:00:58 +0200

    still some bug with gallery

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,36 +21,95 @@ const closeBox = document.querySelector(".icon-x");
 const shadowH3 = document.querySelector(".shadow-h3");
 const smallPicContainer = document.querySelector(".small-pic-container");
 const currentPicture = document.querySelector("#current");
-const smallPictures = document.querySelectorAll(".small-pic");
+const smallPictures = document.getElementsByClassName("small-pic");
 
 const toggleShadowBox = (value) => {
   shadowBox.style.display = value;
 };
 
+// let pictureProt = e.target.parentElement.previousElementSibling.firstChild;
+// let picture = pictureProt.nextElementSibling || e.target;
+// console.log(picture);
+// currentPicture.src = picture.src;
+
+// console.log(smallPictures[i].alt);
+// if (e.target === smallPictures[i]) {
+//   currentPicture.src = e.target.src;
+//   // smallPictures.forEach((img) => (img.style.border = "none"));
+//   // e.target.style.border = " 4px solid #EB8B76";
+// }
+
+// const a = () => {
+//   for (let i = 0; i < smallPictures.length; i++) {
+//     smallPictures[i].src = picture.src.replace(
+//        "0.jpg",
+//         `${smallPictures[i].alt}.jpg`
+//         );
+//       }
+// };
+
+// ||
+//       e.target.parentElement.previousElementSibling === undefined
+
+// smallPictures[i].src = currentPicture.src.replace(
+//   "0.jpg",
+//   `${smallPictures[i].alt}.jpg`
+// );
+
 const clickChecker = (e) => {
   if (e.target.matches(".sec2-btn") || e.target.matches("img")) {
     toggleShadowBox("block");
+
     let txt = e.target.previousElementSibling || e.target;
+    const containerID = [];
+    containerID.push(e.target.parentElement.id);
+
+    console.log(containerID);
 
     if (e.target.previousElementSibling === null) {
       let msg = txt.alt;
       shadowH3.textContent = msg;
+      let picture = e.target;
+      currentPicture.src = picture.src;
+
+      // const containerID = [];
+      // containerID.push(e.target.parentElement.id);
+
+      // console.log(...containerID);
+
+      for (let i = 0; i < smallPictures.length; i++) {
+        smallPictures[
+          i
+        ].src = `assets/img/${containerID[0]}/${smallPictures[i].id}.jpg`;
+      }
     } else {
       let msg = txt.firstChild.nextElementSibling.textContent;
       shadowH3.textContent = msg;
+      let picture =
+        e.target.parentElement.previousElementSibling.firstChild
+          .nextElementSibling;
+      currentPicture.src = picture.src;
+
+      for (let i = 0; i < smallPictures.length; i++) {
+        smallPictures[
+          i
+        ].src = `assets/img/shadow-box-img-1/${smallPictures[i].id}.jpg`;
+      }
     }
   }
 };
 
 const shadowClickChecker = (e) => {
+  console.log("shadow");
   console.log(e.target);
   if (e.target.matches(".small-pic")) {
-    currentPicture.src = e.target.src;
-    smallPictures.forEach((picture) => (picture.style.border = "none"));
+    console.log(e.target);
+    // currentPicture.src = e.target.src.replace("0.jpg", `${e.target.alt}.jpg`);
+    // smallPictures.forEach((img) => (img.style.border = "none"));
     e.target.style.border = " 4px solid #EB8B76";
   }
 };
 
 sectionTwo.addEventListener("click", clickChecker);
 closeBox.addEventListener("click", () => toggleShadowBox("none"));
-shadowBox.addEventListener("click", shadowClickChecker);
+// shadowBox.addEventListener("click", shadowClickChecker);
